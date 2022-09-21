654a509d077ecaf5de1947dbd0c9f6702f72e2b3
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-09-21 12:19:17 +0530

    Initial Commit

---
+++ AnimationSeries/AnimationSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSeriesApp.swift
+//  AnimationSeries
+//
+//  Created by Kanishk Vijaywargiya on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
